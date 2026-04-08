649639db5353db03c26c33191f2b493dfe5a6aa4
Melisa Eugenia Rivas Linares <20245324@esen.edu.sv>
2026-04-07 22:18:35 -0600

    feature: creacion de documentacion para spotlight openapi.spotlight.yaml

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -25,7 +25,7 @@ class RegisterRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', 'unique:users,email'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8'],
             'role' => ['nullable', 'string', Rule::in(['admin', 'bibliotecario', 'estudiante'])],
         ];
     }
